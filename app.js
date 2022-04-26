41f53555a5f9a48c6a5687246022e12507a03b82
Andrzej Barzyk <abarzyk@zstjaslo.pl>
2022-04-26 08:19:39 +0200

    Init commit - template

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+  res.render('index', {
+      layout: false
+  })
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000.');
+})
